1ae6150fb1ba5dc366a8379c6d63877a1c82ed1d
katyssha <118836572+katyssha@users.noreply.github.com>
2023-11-20 12:18:12 +0300

    create this project

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+	employee_id SERIAL PRIMARY KEY,
+	first_name CHARACTER VARYING(100),
+	last_name CHARACTER VARYING(100),
+	title CHARACTER VARYING(100),
+	birth_date DATE,
+	notes TEXT
+)
+
+CREATE TABLE customers (
+	customer_id CHARACTER VARYING(5) PRIMARY KEY,
+	company_name CHARACTER VARYING(100),
+	contact_name CHARACTER VARYING(50)
+)
+
+CREATE TABLE orders (
+	order_id INTEGER PRIMARY KEY,
+	customer_id CHARACTER REFERENCES customers(customer_id),
+	employee_id INTEGER REFERENCES employees(employee_id),
+	order_date DATE,
+	ship_city CHARACTER VARYING(100)
+)
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,75 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+"""Connect to db"""
+conn = psycopg2.connect(host='localhost', database='north',
+                        user='postgres', password='o977kx', )
+
+"""Create cursor"""
+cur = conn.cursor()
+
+"""Execute query"""
+
+
+def csv_reader(filename: str) -> list:
+    """
+    Функция считывает информацию в файле
+    :param filename: строка, название файла
+    :return: список с кортежами
+    """
+    with open(f'north_data/{filename}', encoding='utf-8') as csvfile:
+        all_info = csv.reader(csvfile)
+        info_list = []
+
+        for i in all_info:
+            info_list.append(tuple(i))
+    return info_list
+
+
+def customers() -> None:
+    """
+    Функция заполняет таблицу customers в базе данных
+    :return:
+    """
+    customers_list = csv_reader('customers_data.csv')
+
+    for i in customers_list[1:]:
+        cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', i)
+        conn.commit()
+
+
+def employees() -> None:
+    """
+    Функция заполняет таблицу customers в базе данных
+    :return:
+    """
+    employees_list = csv_reader('employees_data.csv')
+
+    for i in employees_list[1:]:
+        cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', i)
+        conn.commit()
+
+
+def orders() -> None:
+    """
+    Функция заполняет таблицу customers в базе данных
+    :return:
+    """
+    orders_list = csv_reader('orders_data.csv')
+
+    for i in orders_list[1:]:
+        cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', i)
+        conn.commit()
+
+
+orders()
+cur.execute('SELECT * FROM orders')
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+"""Close cursor and connection"""
+cur.close()
+conn.close()
